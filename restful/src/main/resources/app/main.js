f19941858536c1a09930334bb93df73d9e24a711
Sten Roger Sandvik <srs@enonic.com>
2016-09-05 10:31:35 +0200

    Updated to 0.5.0.

--- restful/src/main/resources/app/main.js
+++ restful/src/main/resources/app/main.js
@@ -1,28 +1,20 @@
 // Create a new router.
 var router = require('/lib/router')();
 
-// Adds a filter that adds a custom JSON-based error page.
-router.filter(function (req, next) {
-    var res = next(req);
-
-    if (res.status == 404) {
-        if (res.body) {
-            return res;
-        }
-
-        res.body = {
-            status: 404,
-            message: 'Not found'
-        };
-    }
-
-    return res;
-});
-
 // Set-up the person controller and all routes.
 require('/person/controller.js')(router);
 
 // Serves all requests using the router.
 exports.service = function (req) {
     return router.dispatch(req);
+};
+
+// Handle error
+exports.handleError = function (error) {
+    return {
+        body: {
+            status: error.status,
+            message: error.message
+        }
+    };
 };
